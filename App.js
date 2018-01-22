7081ed45fac46076a7b9c5b767741045e8077330
Chris Mears <chris.mears@gmail.com>
2018-01-22 14:56:06 -0800

    Add state to App.js

--- App.js
+++ App.js
@@ -1,31 +1,49 @@
 import React from 'react'
 import { StyleSheet, Text, ScrollView, View } from 'react-native'
+import uuidv4 from 'uuid/v4'
 import EditableTimer from './components/EditableTimer'
 import ToggleableTimerForm from './components/ToggleableTimerForm'
 
 export default class App extends React.Component {
+  state = {
+    timers: [
+      {
+        title: 'Study React Native',
+        project: 'Education',
+        id: uuidv4(),
+        elapsed: 5456009,
+        isRunning: true,
+      },
+      {
+        title: 'Write blog post',
+        project: 'Writing',
+        id: uuidv4(),
+        elapsed: 3246,
+        isRunning: false,
+      }
+    ]
+  }
+
   render() {
+    const { timers} = this.state
+
     return (
       <View style={styles.appContainer}>
         <View style={styles.titleContainer}>
           <Text style={styles.title}>Timers</Text>
         </View>
         <ScrollView style={styles.timerList}>
-          <ToggleableTimerForm isOpen={false}/>
-          <EditableTimer
-            id="1"
-            title="Mow the lawn"
-            projects="House Chores"
-            elapsed="8986300"
-            isRunning
-          />
-          <EditableTimer
-            id="2"
-            title="Bake squash"
-            projects="Kitchen Chores"
-            elapsed="3890985"
-            editFormOpen
-          />
+          <ToggleableTimerForm />
+          {timers.map(({ title, project, id, elapsed, isRunning }) => (
+            <EditableTimer
+              key={id}
+              id={id}
+              title={title}
+              projects={project}
+              elapsed={elapsed}
+              isRunning={isRunning}
+            />
+          ))}
         </ScrollView>
       </View>
     );
