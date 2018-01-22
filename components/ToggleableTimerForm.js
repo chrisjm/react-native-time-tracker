ec8f673d6842a16967f90c4a4745c111ec42710f
Chris Mears <chris.mears@gmail.com>
2018-01-22 15:26:03 -0800

    Add state to ToggleableTimerForm

--- components/ToggleableTimerForm.js
+++ components/ToggleableTimerForm.js
@@ -3,12 +3,24 @@ import { StyleSheet, View } from 'react-native'
 import TimerButton from './TimerButton'
 import TimerForm from './TimerForm'
 
-export default function ToggleableTimerForm({ isOpen }) {
-  return (
-    <View style={[styles.container, !isOpen && styles.buttonPadding]}>
-      {isOpen ? <TimerForm/> : <TimerButton title="+" color="black"/>}
-    </View>
-  )
+export default class ToggleableTimerForm extends React.Component() {
+  state = {
+    isOpen: false,
+  }
+
+  handleFormOpen = () => {
+    this.setState({ isOpen: true })
+  }
+
+  render() {
+    const { isOpen } = this.state
+
+    return (
+      <View style={[styles.container, !isOpen && styles.buttonPadding]}>
+        {isOpen ? <TimerForm/> : <TimerButton title="+" color="black"/>}
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
